abe95f543d862bcf34ab37279e909a6cc5116ae6
jatin.rajpura <prensoni0143@yahoo.com>
2018-04-24 14:23:21 +0800

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,9 @@
+name = "My Name is Jatin"
+car = "Jatin's car is purple"
+dog = 'Dog was named as "Love" '
+double = "He liked cat's baby who was named as \"Lily\" "
+
+print(name)
+print(car)
+print(dog)
+print(double)
